2aedb87902391aea6a359297a3b1a2c44c4259f7
k9732 <k97328aa@gmail.com>
2024-08-02 22:30:45 +0900

    project maintool 합쳤습니다

--- WebContent/js/project.js
+++ WebContent/js/project.js
@@ -11,8 +11,8 @@ $(function() {
 				//alert(response.result);   // success
 				alert("즐겨찾기");
 				let clone_object = _this.parent().clone();
-				$("#bookmark").append(clone_object);  // 마지막 자식으로~
-				$("#bookmark > div:last-child .star").removeClass("star").html('<img class="yellow_star" src="https://flow.team/flow-renewal/assets/images/icons/icon_star_on.png?v=ca949083bd3e2d74e7125167485cff818959483a">');
+				$("#project_bookmark").append(clone_object);  // 마지막 자식으로~
+				$("#project_bookmark > div:last-child .star").removeClass("star").html('<img class="yellow_star" src="https://flow.team/flow-renewal/assets/images/icons/icon_star_on.png?v=ca949083bd3e2d74e7125167485cff818959483a">');
 				_this.parent().remove();  // 이전 것은 삭제~
 			},
 			error: function(){
@@ -20,7 +20,7 @@ $(function() {
 			}
 		});
 	});
-	$(document).on("click", "#bookmark .yellow_star", function() {
+	$(document).on("click", "#project_bookmark .yellow_star", function() {
 		let _this = $(this);
 		let project_idx = $(this).parent().parent().attr("project_idx");
 		$.ajax({
